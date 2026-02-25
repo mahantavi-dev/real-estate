339fca493b90387fa0af8dd785ee859ca16b4498
Avi Mahant <Avi.avikshitmahant@gmail.com>
2026-02-25 21:52:14 +0530

    Fix case sensitivity for Linux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import About from './components/about'
-import Header from './components/header'
+import About from './components/About'
+import Header from './components/Header'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
